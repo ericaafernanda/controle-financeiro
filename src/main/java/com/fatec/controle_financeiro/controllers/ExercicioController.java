4077b14e7cfa632cd3489342bdded057c0415f55
ericaafernanda <ericaferoliveira265@gmail.com>
2024-08-21 19:40:11 -0300

    parametros

--- src/main/java/com/fatec/controle_financeiro/controllers/ExercicioController.java
+++ src/main/java/com/fatec/controle_financeiro/controllers/ExercicioController.java
@@ -1,15 +1,38 @@
 package com.fatec.controle_financeiro.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
-
+@RequestMapping("/api/exercicios1")
 public class ExercicioController {
 
     @GetMapping
     public String HelloWorldId(){
         return "Hello";
     }
+
+    //@PathVariable = anotação para definir que a variável nome é do
+    //tipo String será recebida pelo parâmetro{nome}
+    //WEB - http://localhost:8090/api/exercicios1/reverter-nome/erica
+    @GetMapping("/reverter-nome/{nome}")
+    public String reverternome(@PathVariable String nome) {
+        return new StringBuilder(nome).reverse().toString();
+    }
+
+    // WEB - http://localhost:8090/api/exercicios1/par-ou-impar/6
+    @GetMapping("/par-ou-impar/{numero}")
+    public String verificarParOuImpar(@PathVariable Integer numero) {
+        if (numero % 2 == 0) {
+            return "Par";
+        } else{
+            return "Ímpar";
+        }        
+    }
+    
     
 }
