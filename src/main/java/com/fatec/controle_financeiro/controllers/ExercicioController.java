1cc9c9a7a3d03f1cc7937f28f007f6a9d3042de4
ericaafernanda <ericaferoliveira265@gmail.com>
2024-08-29 22:40:12 -0300

    aula_29_08

--- src/main/java/com/fatec/controle_financeiro/controllers/ExercicioController.java
+++ src/main/java/com/fatec/controle_financeiro/controllers/ExercicioController.java
@@ -2,6 +2,8 @@ package com.fatec.controle_financeiro.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,8 +21,15 @@ public class ExercicioController {
     //@PathVariable = anotação para definir que a variável nome é do
     //tipo String será recebida pelo parâmetro{nome}
     //WEB - http://localhost:8090/api/exercicios1/reverter-nome/erica
+    
     @GetMapping("/reverter-nome/{nome}")
     public String reverternome(@PathVariable String nome) {
+        return new StringBuilder(nome).reverse().toString();
+    }
+
+    //Com requisição de corpo(parametro)
+    @PostMapping("/reverter-nome-requisicao-corpo")
+    public String reverterNomeRequisicaoCorpo(@RequestBody String nome) {
         return new StringBuilder(nome).reverse().toString();
     }
 
@@ -54,4 +63,23 @@ public class ExercicioController {
         }
     }
     
+    
+    public class User {
+        private String nome;
+        private Integer idade;
+        public String getNome() {
+            return nome;
+        }
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+        public Integer getIdade() {
+            return idade;
+        }
+        public void setIdade(Integer idade) {
+            this.idade = idade;
+        }
+            
+        
+    }
 }
